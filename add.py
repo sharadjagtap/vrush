662d1163a34390e9301427aa45b5f8b85e428c36
sharadjagtap <sharadjagtap19@gmail.com>
2019-12-12 21:02:19 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,9 @@
+
+
+class math:
+    def addition(a,b):
+        return a+b
+
+
+
+# print(addition(2,3))
---
+++ new_add.py
@@ -0,0 +1,11 @@
+from Monkey_Patching.add import *
+
+
+class new:
+
+    def new_add(a,b):
+        return a-b
+
+math.addition=new.new_add
+
+print(math.addition(2,4))
